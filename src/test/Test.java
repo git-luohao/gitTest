84fe38373d8b226cca46565dd1fa76681e231fc5
git-luohao <3054520836@qq.com>
2019-08-28 16:11:36 +0800

    添加了一个项目

---
+++ src/test/Test.java
@@ -0,0 +1,16 @@
+package test;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		Test test = new Test();
+		test.a();
+	}
+	
+	public void a() {
+		System.out.println(1111);
+		
+	}
+	
+	
+}
